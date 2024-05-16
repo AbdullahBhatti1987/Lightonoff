f1d11fc9f2870c59af67717fd688105725ba1ff6
AbdullahBhatti1987 <156850703+AbdullahBhatti1987@users.noreply.github.com>
2024-05-16 09:48:54 +0500

    Update filename

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var button = document.getElementById("button");
 button.addEventListener("click", function () {
     console.log('lights off thi');
     if (button.value == "off") {
-        bulb.src = "./images/gardenlighton.jpg";
+        bulb.src = "./images/gardenlighton.JPG";
         button.src = "./images/offbutton.png";
         button.value = 'on';
     } else {
         console.log('lights on thi');
-        bulb.src = "./images/gardenlightoff.jpg";
+        bulb.src = "./images/gardenlightoff.JPG";
         button.src = "./images/onbutton.png";
         button.value = "off";
 }
